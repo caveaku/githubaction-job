70644f3107a1175221e05ec0a89c07ce01d402be
unknown <caveaku>
2024-12-04 18:48:42 -0500

    update repo

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "default" {
   engine             = "postgres"
   engine_version     = var.engine_version
   instance_class     = var.instance_class
-  name               = var.db_name
+  db_name               = var.db_name
   username           = var.db_username
   password           = var.db_password
   db_subnet_group_name = aws_db_subnet_group.default.name
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
 
 db_name       = "mydatabase"
 db_username   = "admin"
-db_password   = "yourpassword"
-subnet_ids    = ["subnet-12345678", "subnet-87654321"]
+db_password   = "Ajesam2024##"
+#subnet_ids    = ["subnet-12345678", "subnet-87654321"]
