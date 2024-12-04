8fff1f10b1f43e74c6d8d0da7b0e29ceac728c0a
caveaku <85325018+caveaku@users.noreply.github.com>
2024-12-04 18:40:21 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,9 @@
+provider "aws" {
+  region = var.region
+}
 
+variable "region" {
+  description = "The AWS region to provision the resources."
+  type        = string
+  default     = "us-east-1"
+}
